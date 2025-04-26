9080bdedb664e00221c6d4688ecc8134f5906799
addy <addyreearc@gmail.com>
2025-04-26 18:29:55 +0200

    pointers

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 #define STBI_NO_STDIO
 #include "../includes/stb_image.h"
 
+enum imgformat
+{
+	png = 0, jpeg = 1, webp = 2, heic = 3
+};
+
 int main(void)
 {
 	std::cout << "Hello" << std::endl;
@@ -13,10 +18,15 @@ int main(void)
 
 extern "C"
 {
-	void image_info(uint8_t* byte_array, int bytes)
+	void Decode(uint8_t* bytes, int size, imgformat format, uint8_t** decoded_pixel_ptr, int* decoded_width_ptr, int* decoded_height_ptr, int* decoded_channels_ptr)
 	{
 		int width, height, channels;
-		stbi_info_from_memory(byte_array, bytes, &width, &height, &channels);
+		stbi_info_from_memory(bytes, size, &width, &height, &channels);
+
+		*decoded_pixel_ptr = 0;
+		*decoded_width_ptr = width;
+		*decoded_height_ptr = height;
+		*decoded_channels_ptr = channels;
 
 		std::cout << "Width: " << width << std::endl;
 		std::cout << "Height: " << height << std::endl;
