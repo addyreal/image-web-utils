bb3ca8f7e2e1b4bb3903fe6f999e8f769f3dba6a
addy <addyreearc@gmail.com>
2025-04-27 22:34:27 +0200

    revert without annoying git thing

--- logic.js
+++ logic.js
@@ -45,32 +45,11 @@ function bytesToImageFormat(bytes)
 	{
         return 2;
     }
-<<<<<<< HEAD
-	// ftyp heic
-	else if((bytes[4] === 0x66 && bytes[5] === 0x74 && bytes[6] === 0x79 && bytes[7] === 0x70) && (bytes[8] === 0x68 && bytes[9] === 0x65 && bytes[10] === 0x69 && bytes[11] === 0x63))
-	{
-		return 3;
-	}
-	//ftyp mif1 and heic as minor
-	else if((bytes[4] === 0x66 && bytes[5] === 0x74 && bytes[6] === 0x79 && bytes[7] === 0x70) && (bytes[8] === 0x6D && bytes[9] === 0x69 && bytes[10] === 0x66 && bytes[11] === 0x31))
-	{
-		console.log("checking minors");
-		for(let i = 16; i + 3 < bytes.length && i < 48; i += 4)
-		{
-			if(bytes[i] == 0x68 && bytes[i+1] == 0x65 && bytes[i+2] == 0x69 && bytes[i+3] == 0x63)
-			{
-				return 3;
-			}
-		}
-		console.log("done checking minors");
-		return -1;
-=======
 	// heic (f,t,y,p h,e,i,c)
 	else if(bytes[4] === 0x66 && bytes[5] === 0x74 && bytes[6] === 0x79 && bytes[7] === 0x70 &&
 		bytes[20] === 0x68 && bytes[21] === 0x65 && bytes[22] === 0x69 && bytes[23] === 0x63)
 	{
 		return 3;
->>>>>>> parent of 597e8ed (better debug)
 	}
 	// unsupported
 	else
